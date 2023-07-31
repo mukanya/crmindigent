3d66310251fbecf5480989a47e393b703d0bc4df
Brian Gondo <briangondo@gmail.com>
2023-07-31 16:48:38 +0200

    Update runserver.py port 8080 to 432

--- runserver.py
+++ runserver.py
@@ -4,4 +4,4 @@ from crmindigent.wsgi import application
 # documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
 
 if __name__ == '__main__':
-    serve(application, host = 'localhost', port='8080')
+    serve(application, host = 'localhost', port='5432')
